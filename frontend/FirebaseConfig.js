d83adcc77929208b17a77aa5cb2505ff5879a6f1
Ameat77 <amit.krishnaiyer@gmail.com>
2024-03-25 01:09:31 -0500

    fixed major security issue

--- frontend/FirebaseConfig.js
+++ frontend/FirebaseConfig.js
@@ -1,20 +1,24 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID
+} from '@env';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD0Ap1uUFHhCRHMt2_531kVOg7FCgREj3M",
-  authDomain: "illinigo-d848b.firebaseapp.com",
-  projectId: "illinigo-d848b",
-  storageBucket: "illinigo-d848b.appspot.com",
-  messagingSenderId: "1098672997563",
-  appId: "1:1098672997563:web:bf8f143175026b3bcc559e"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID
 };
-
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
--- frontend/app/screens/Profile.jsx
+++ frontend/app/screens/Profile.jsx
@@ -1,24 +1,79 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { StyleSheet, View, TextInput, FlatList, Image, Text, SafeAreaView  } from 'react-native';
+import DATA from '../../database.json'
+
+
+
+
+
+const Item = ({ image }) => (
+  <View style={styles.item}>
+    <Image source={image} style={styles.image} />
+  </View>
+);
 
 const Profile = () => {
+  const totalItems = DATA.length;
+  const foundItems = DATA.filter(item => item.found).length;
+
+  console.log(DATA)
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Profile</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.catalogTitle}>Catalog</Text>
+        <Text style={styles.foundText}>Found: {foundItems}/{totalItems}</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <TextInput
+          placeholder="Search for a critter"
+          style={styles.searchInput}
+        />
+      </View>
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) => <Item image={item.image} />}
+        keyExtractor={item => item.id}
+        numColumns={2}
+      />
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5f5f5', // Light gray background
+    marginTop: 20,
   },
-  text: {
-    fontSize: 24, // Large text size
-    fontWeight: 'bold', // Bold text for better visibility
+  headerContainer: {
+    paddingHorizontal: 10,
+    paddingTop: 20,
+  },
+  catalogTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  foundText: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  searchContainer: {
+    padding: 10,
+  },
+  searchInput: {
+    height: 40,
+    borderWidth: 1,
+    padding: 10,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    margin: 8,
+  },
+  image: {
+    width: 100,
+    height: 100,
   },
 });
 
