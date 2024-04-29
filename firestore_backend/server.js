d6fc88f0cb8bca0a0dd26f6983a39f91e3b36458
Ameat77 <amit.krishnaiyer@gmail.com>
2024-04-29 17:48:48 -0500

    firestore backend

--- firestore_backend/server.js
+++ firestore_backend/server.js
@@ -52,6 +52,48 @@ app.get('/user/:userId', async (req, res) => {
     }
 });
 
+app.put('/user/:userId/update', async (req, res) => {
+  const userId = req.params.userId;
+  const { found } = req.body;
+
+  try {
+    const userDoc = await db.collection('users').doc(userId).get();
+
+    if (!userDoc.exists) {
+      return res.status(404).send("User not found");
+    }
+    await db.collection('users').doc(userId).update({
+      found: found
+    });
+
+    return res.status(200).send("User updated successfully");
+  } catch (error) {
+    console.error("Error updating user data", error);
+    return res.status(500).send(error);
+  }
+});
+
+app.put('/user/:userId/updateImage', async (req, res) => {
+  const userId = req.params.userId;
+  const { pictures } = req.body;
+
+  try {
+    const userDoc = await db.collection('users').doc(userId).get();
+
+    if (!userDoc.exists) {
+      return res.status(404).send("User not found");
+    }
+    await db.collection('users').doc(userId).update({
+      pictures: pictures
+    });
+
+    return res.status(200).send("User updated successfully");
+  } catch (error) {
+    console.error("Error updating user data", error);
+    return res.status(500).send(error);
+  }
+});
+
 const addUserWithId = async (userId, userData) => {
     try {
       const docRef = db.collection('users').doc(userId);
